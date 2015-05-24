a74673d0ccc0679887d17155b868491ba50ed5c5
Julien B <julien.bonnetaud@gmail.com>
2015-05-24 12:27:21 +0200

    fix nav

--- www/js/app.js
+++ www/js/app.js
@@ -1,3 +1,113 @@
+var appIndex = $.inherit({
+
+    duration: 500,
+    sideBarOpent: false,
+    friendOpen: false,
+
+    __constructor : function() {
+        this.body = $('body');
+        this.sidebar = this.body.find('.sidebar');
+        this.btnMenu = this.body.find('a.display-sidebar');
+        this.sidebarSize = this.sidebar.width();
+
+        this.friendsPanel = this.body.find('.friend-panel-mini');
+        this.friendsPanelSize = this.friendsPanel.outerHeight();
+        this.friendsPanelOpenSize = this.friendsPanel.find('.friend-open-panel').outerHeight();
+        this.btnRecover = this.friendsPanel.find('.recover');
+        this.btnDue = this.friendsPanel.find('.due');
+
+        this.initEvents();
+    },
+
+    initEvents : function() {
+        this.initMenu();
+        this.initFriends();
+    },
+
+    initMenu : function() {
+        var self = this;
+
+        // MENU EVENT
+        this.btnMenu.on('click', function (e) {
+            e.preventDefault();
+            console.log(self.sidebarSize);
+            if (self.sidebarOpen){
+                self.closeMenu();
+            } else {
+                self.openMenu();
+            }
+        })
+    },
+
+    initFriends : function() {
+        var self = this;
+
+        this.friendsPanel.delegate(this.btnRecover, 'click', function(e) {
+            // Recuperer div parent et l'attr data-id pour savoir quel friend modifier
+            e.preventDefault();
+            var action = $(e.target).attr('class');
+            var panel = $(e.target).parent('.friend-panel-mini');
+            var data = {
+                'action': action,
+                'panel': panel
+            };
+            if (data.panel.hasClass('active')) {
+                self.closeFriendPanel(data);
+            } else {
+                // Affiche popin de formulaire
+                if (action == 'recover') {
+                    self.friendsPanel.height(self.friendsPanelSize);
+                    self.friendsPanel.removeClass('active');
+                    console.log("Affiche recover panel");
+                    self.openFriendPanel(data);
+                }
+                if (action == 'due') {
+                    self.friendsPanel.height(self.friendsPanelSize);
+                    self.friendsPanel.removeClass('active');
+                    self.friendsPanel.height(self.friendsPanelSize);
+                    console.log("Affiche due panel");
+                }
+            }
+        })
+    },
+
+    closeFriendPanel : function(data) {
+        $(data.panel).stop().animate({
+            'height': this.friendsPanelSize,
+            'ease': 'ease'
+        }, 300);
+        $(data.panel).removeClass('active');
+    },
+
+    openFriendPanel : function(data) {
+        console.log(data.id + " est l'ID du parent");
+        this.friendsPanel.height(this.friendsPanelSize);
+        var openFriendPanelSize = this.friendsPanelSize + this.friendsPanelOpenSize;
+
+        $(data.panel).stop().animate({
+            'height': openFriendPanelSize,
+            'ease': 'ease'
+        }, 300);
+        $(data.panel).addClass('active');
+    },
+
+    closeMenu : function() {
+        this.body.stop().animate({
+            'left': 0
+        }, this.duration);
+        this.sidebarOpen = false;
+    },
+
+    openMenu : function() {
+        this.body.stop().animate({
+            'left': this.sidebarSize
+        }, this.duration);
+        this.sidebarOpen = true;
+    }
+});
+new appIndex();
+
+/*
 $(document).ready(function() {
     var body = $('body');
     var sidebar = body.find('.sidebar');
@@ -57,7 +167,6 @@ $(document).ready(function() {
                 console.log("Affiche due panel");
             }
         }
-
     })
 
     function openFriendPanel(data) {
@@ -79,3 +188,4 @@ $(document).ready(function() {
         $(data.panel).removeClass('active');
     }
 })
+*/
